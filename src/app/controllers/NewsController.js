11874a13d0f43b54a1e2ec3cd963c5dd0c070744
ngocsnake <ngocsnake@gmail.com>
2022-05-14 17:00:47 +0700

    commit

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,12 +1,14 @@
 var rp = require('request-promise');
+const axios = require('axios').default;
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
 class NewsController {
     // [GET] /news
     async index(req, res, next) {
-        const html = await fetch('https://covid19.gov.vn/chi-dao-chong-dich.htm')
-            .then(res => res.text())
+        // const html = await fetch('https://covid19.gov.vn/chi-dao-chong-dich.htm')
+        const html = await axios.get('https://covid19.gov.vn/chi-dao-chong-dich.htm')
+            .then(response => response.data)
             .then(
                 res => {
 
@@ -24,8 +26,8 @@ class NewsController {
         const link = 'https://covid19.gov.vn/' + req.params.detail;
 
 
-        const html = await fetch(link)
-            .then(response => response.text())
+        const html = await axios.get(link)
+            .then(response => response.data)
             .then((response) => {
 
                 const dom = new JSDOM(response);
